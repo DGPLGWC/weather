8a20823c408ffa2e047e94a8e4fa487ccedf7114
rsreddy <radha21@gmail.com>
2017-02-22 20:55:58 +0000

    added tempaltes

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return 'Hello World'
+def index():
+    return 'Hello Girls Who Code!'
+
+@app.route('/hello/<string:name>')
+def hello(name):
+    return render_template(
+        'helloname.html', name=name)
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
