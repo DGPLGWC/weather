0b54353a379fcea03a30cf3b6a7870810ff1b318
rsreddy <radha21@gmail.com>
2017-04-19 22:18:45 +0000

    interaction between html and python code

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ def index():
     return render_template(
         'index.html')
 
+@app.route('/get-weather')
+def get_weather():
+    return render_template(
+        'weather.html',
+        location='Downers Grove',
+        temp='60',
+        month='April',
+        description='rain')
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
